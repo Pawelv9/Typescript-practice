184a4dc1079ba31c1d4a19721fe9d846c0d652c2
Pavel <Pawelv9@gmail.com>
2019-08-15 03:40:35 +0300

    added sort for sting

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
-const numbersCollection = new NumbersCollection([9, 5, 8, -1]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([9, 5, 8, -1]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('tXaamn');
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
