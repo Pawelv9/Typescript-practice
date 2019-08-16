09526a7f9fd6a12232a95d86855b59a44c29c391
Pavel <Pawelv9@gmail.com>
2019-08-16 23:37:50 +0300

    added linked list implementation

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,13 +1,26 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
+
 
 // const numbersCollection = new NumbersCollection([9, 5, 8, -1]);
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 // console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection('tXaamn');
-const sorter = new Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection('tXaamn');
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(400);
+linkedList.add(8);
+linkedList.add(-4);
+linkedList.add(90);
+linkedList.add(50);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data);
+linkedList.print();
